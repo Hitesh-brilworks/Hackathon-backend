dd746be026e31cede59c3cb8bf0768930a3727fb
Hitesh Chaudhary <hitesh.c@brilworks.com>
2025-07-19 10:59:57 +0530

    added change

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const userRoutes = require("./userRoutes");
 const exerciseRoutes = require("./exerciseRoutes");
 const authRoutes = require("./authRoutes");
+const workoutRoutineRoutes = require("./workoutRoutineRoutes");
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
 router.use("/auth", authRoutes);
 router.use("/exercises", exerciseRoutes);
+router.use("/routines", workoutRoutineRoutes);
 
 module.exports = router;
