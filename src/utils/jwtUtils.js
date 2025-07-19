3f53fe7226387b5bb96c47ecf63e940da84c11da
Hitesh Chaudhary <hitesh.c@brilworks.com>
2025-07-19 14:18:26 +0530

    added changes

--- src/utils/jwtUtils.js
+++ src/utils/jwtUtils.js
@@ -3,9 +3,7 @@ const { createError } = require("./errorUtils");
 
 const generateToken = (payload) => {
   try {
-    return jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_EXPIRES_IN || "1h",
-    });
+    return jwt.sign(payload, process.env.JWT_SECRET);
   } catch (error) {
     throw createError("Error generating token", 500, error);
   }
